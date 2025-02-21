42529c5546a3d636879ae4d5f7e5094132c0567a
Andriy Klymenko <andriy.klymenko@sigma.software>
2025-02-21 16:12:28 +0200

    Add groovy script.

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,18 @@
+import groovy.io.FileType;
+import java.io.File;
+
+def call(Map config=[:]){
+    def dir = new File(pwd());
+
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') { 
+        writer -> dir.eachFileRecurse(FileType.ANY){
+            file ->
+            if (file.isDirectory()){
+                 writer.writeLine(file.name);            
+            } else {
+                writer.writeLine('\t' + file.name + '\t' + file.length());
+            }
+        } 
+    }
+
+}
