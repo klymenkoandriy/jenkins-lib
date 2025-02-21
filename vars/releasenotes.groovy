903ece8f2014e4df04d714a044d4390fbf2ae6e5
Andriy Klymenko <andriy.klymenko@sigma.software>
2025-02-21 17:32:37 +0200

    Fix windows error.

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,17 +2,17 @@ import groovy.io.FileType;
 import java.io.File;
 
 def call(Map config=[:]){
-    def dir = new File(pwd());
+    def dir = new File(".");
 
-    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') { 
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') {
         writer -> dir.eachFileRecurse(FileType.ANY){
             file ->
             if (file.isDirectory()){
-                 writer.writeLine(file.name);            
+                 writer.writeLine(file.name);
             } else {
                 writer.writeLine('\t' + file.name + '\t' + file.length());
             }
-        } 
+        }
     }
 
 }
